0de0c89a33a29c4e80534d7024b096e548263d6b
David Pugh <djpugh@gmail.com>
2018-08-29 10:48:35 +0100

    Updating yaml

--- src/mtfit/_version.py
+++ src/mtfit/_version.py
@@ -43,7 +43,7 @@ def get_config():
     cfg.style = "pep440"
     cfg.tag_prefix = "v"
     cfg.parentdir_prefix = "MTfit-"
-    cfg.versionfile_source = "src/_version.py"
+    cfg.versionfile_source = "src/MTfit/_version.py"
     cfg.verbose = False
     return cfg
 
