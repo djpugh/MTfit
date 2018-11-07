1aa80d0201bdff2bed82a57edb81b8ec8c78cff8
David Pugh <djpugh@gmail.com>
2018-11-07 20:49:09 +0000

    Adding travis/appveyor integration

--- ci/get_appveyor_wheels.py
+++ ci/get_appveyor_wheels.py
@@ -1,22 +1,53 @@
 from __future__ import unicode_literals, print_function
 
-import os.path
+import os
+import re
 
 import requests
+REPO_PATH = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+
 
 api_url = 'https://ci.appveyor.com/api'
 account_name = os.getenv('APPVEYOR_ACCOUNT')
 project_slug = os.getenv('APPVEYOR_SLUG')
 headers = {'Authorization': 'Bearer ' + os.getenv('APPVEYOR_TOKEN')}
+travis_headers = {'Authorization': 'token ' + os.getenv('TRAVIS_TOKEN'), 'Travis-API-Version': '3'}
 
+appveyor_version = None
+
+# This is a real messy thing here to get the appveyor version
+# we are going to go back over the test stage job logs and look for the line
+# Started AppVeyor build (buildId={buildId}, version={version})
+# To start with lets get the jobs
+travis_api_url = 'https://api.travis-ci.org'
+travis_build_stage_url = '{}/build/{}/jobs'.format(travis_api_url, os.getenv('TRAVIS_JOB_ID'))
+r = requests.get(travis_build_stage_url, headers=travis_headers)
+r.raise_for_status()
+jobs = r.json()
+job_ids = [u['id'] for u in jobs['jobs'] if u['stage']['name'].lower() == 'test']
+# Now we need to get the logs and check if that line is in it
+for id_ in job_ids:
+    travis_logs_url = '{}/job/{}/log'.format(travis_api_url, id_)
+    r = requests.get(travis_logs_url, headers=travis_headers)
+    r.raise_for_status()
+    # So we try to split on our search string
+    log = r.json()['content']
+    appveyor_versions = re.findall("Started AppVeyor build \(buildId=.*$", log, re.MULTILINE)
+    if appveyor_versions:
+        appveyor_version = appveyor_versions[0]
+        break
+
+if appveyor_version is None:
+    raise RuntimeError('No appveyor build found in test stage')
 
 # Download the artifacts to wheelhouse/
-build_info_url = '{}/projects/{}/{}/build/{}'.format(api_url, account_name, project_slug, os.getenv('TRAVIS_COMMIT'))
+build_info_url = '{}/projects/{}/{}/build/{}'.format(api_url, account_name, project_slug, appveyor_version)
 r = requests.get(build_info_url)
 r.raise_for_status()
 build = r.json()
 job_ids = [job['jobId'] for job in build['jobs']]
-os.mkdir('dist')
+if not os.path.exists(os.path.join(REPO_PATH, 'dist'))
+os.mkdir(os.path.join(REPO_PATH, 'dist'))
 for job_id in job_ids:
     r = requests.get('{}/buildjobs/{}/artifacts'.format(api_url, job_id), headers=headers)
     r.raise_for_status()
@@ -24,8 +55,9 @@ for job_id in job_ids:
         url = '{}/buildjobs/{}/artifacts/{}'.format(api_url, job_id, artifact['fileName'])
         r = requests.get(url, headers=headers, stream=True)
         r.raise_for_status()
-        file_name = 'dist/' + os.path.basename(artifact['fileName'])
+        file_name = os.path.join(REPO_PATH, 'dist', os.path.basename(artifact['fileName']))
         with open(file_name, 'wb') as f:
             for chunk in r.iter_content(None):
                 f.write(chunk)
+            f.close()
 print('Downloaded ' + f.name)
--- ci/travis_appveyor_build.py
+++ ci/travis_appveyor_build.py
@@ -6,6 +6,7 @@ import time
 
 import requests
 
+
 api_url = 'https://ci.appveyor.com/api'
 account_name = os.getenv('APPVEYOR_ACCOUNT')
 project_slug = os.getenv('APPVEYOR_SLUG')
@@ -18,55 +19,55 @@ class Log:
         self.job_status = {}
         self._log = {}
 
-    def check_job(self, job_id, job_status):
+    def update_job(self, job_id, job_status):
         current_status = self.job_status.get(job_id, 'queued')
-        if current_status == 'success' and job_status == 'success':
-            log_output = ''
-        elif job_status != 'queued':
+        if job_status != 'queued' and not (current_status == 'success' and job_status == 'success'):
             # We need to get and update the log here
             r = requests.get('{}/buildjobs/{}/log'.format(api_url, job_id))
-
             r.raise_for_status()
             new_log = r.content.decode()
             log_diff = new_log.replace(self._log.get(job_id, ''), '').lstrip()
             self._log[job_id] = new_log
-            log_output = log_diff
-        else:
-            log_output = ''
+            print(log_diff.strip('/r').strip())
         self.job_status[job_id] = current_status
-        return log_output
 
 
 # Trigger the AppVeyor build
-r = requests.post(api_url + '/builds', {
+payload = {
     'accountName': account_name,
     'projectSlug': project_slug,
     'branch': os.getenv('TRAVIS_BRANCH'),
     'commitID': os.getenv('TRAVIS_COMMIT')
-}, headers=headers)
+}
+r = requests.post(api_url + '/builds', payload, headers=headers)
 r.raise_for_status()
 build = r.json()
 print('Started AppVeyor build (buildId={buildId}, version={version})'.format(**build))
 log = Log()
 # Wait until the build has finished
+has_responded = False
 while True:
     url = '{}/projects/{}/{}/build/{}'.format(api_url, account_name, project_slug,
                                               build['version'])
     r = requests.get(url, headers=headers)
-    r.raise_for_status()
-    build = r.json()['build']
-    status = build['status']
-    if status in ('queued'):
-        print('Build status: {}; checking again in 10 seconds'.format(status))
-        time.sleep(10)
-    elif status in ('running'):
-        # Get the logs and update here
-        for job in build['jobs']:
-            print(log.check_job(job['jobId'], job['status']))
-        time.sleep(10)
-    elif status == 'success':
-        print('Build successful')
-        break
+    if r.status_code == 404 and not has_responded:
+        time.sleep(5)
     else:
-        print('Build failed with status: {}'.format(status), file=sys.stderr)
-        sys.exit(1)
+        has_responded = True
+        r.raise_for_status()
+        build = r.json()['build']
+        status = build['status']
+        if status in ('queued'):
+            print('Build status: {}; checking again in 10 seconds'.format(status))
+            time.sleep(10)
+        elif status in ('running'):
+            # Get the logs and update here
+            for job in build['jobs']:
+                log.update_job(job['jobId'], job['status'])
+            time.sleep(10)
+        elif status == 'success':
+            print('Build successful')
+            break
+        else:
+            print('Build failed with status: {}'.format(status), file=sys.stderr)
+            sys.exit(1)
